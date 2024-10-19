2d859009f6662479c29cb79871344c2a92606f14
richiexue <richard772794659@gmail.com>
2024-10-19 13:08:15 -0700

    setup project

---
+++ src/BuildingBlocks/Contracts/EventBus.Messages/AuctionDeleted.cs
@@ -0,0 +1,6 @@
+namespace BuildingBlocks.Contracts.EventBus.Messages;
+
+public class AuctionDeleted
+{
+    public string Id { get; set; }
+}
---
+++ src/BuildingBlocks/Contracts/EventBus.Messages/AuctionFinished.cs
@@ -0,0 +1,10 @@
+namespace BuildingBlocks.Contracts.EventBus.Messages;
+
+public class AuctionFinished
+{
+    public bool ItemSold { get; set; }
+    public string AuctionId { get; set; }
+    public string Winner { get; set; }
+    public string Seller { get; set; }
+    public int? Amount { get; set; }
+}
